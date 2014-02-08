b44e2c49ccaef71cf1d696d7aec55d214adb0928
Matthew Hamel <matt@codepl.us>
2014-02-07 19:41:19 -0500

    Add add_alias.php helper

---
+++ helpers/add_alias.php
@@ -0,0 +1,77 @@
+<?php
+
+    /**
+     * Argument 1
+     * The alias name to be added.
+     * Example: "Carbon"
+     *
+     * @var string
+     */
+    $name = $argv[1];
+
+    /**
+     * Argument 2
+     * Path to the facade. Use quotes.
+     * Example: "Carbon\Carbon"
+     *
+     * @var string
+     */
+    $value = $argv[2];
+
+    /**
+     * Argument 3
+     * The path and filename of the config file.
+     * Example: "app/config/app.php"
+     *
+     * @var string
+     */
+    $file = $argv[3];
+
+    /**
+     * Read each line of file to an array.
+     */
+    $lines = file($file);
+
+    $output = '';
+    $in_array = false;
+
+    /**
+     * Loop over file lines. Quick and dirty.
+     */
+    foreach ($lines as $line_num => $line) {
+        if (! $in_array && strpos($line, "'aliases' => array") !== false) {
+            $in_array = true;
+        }
+
+        if ($in_array && strpos($line, $value) !== false) {
+            break;
+        }
+
+        if ($in_array && strpos($line, ')') !== false) {
+            $in_array = false;
+
+            $output = rtrim($output);
+
+            $prefix = '';
+            $suffix = '';
+            if (substr($value, -1) == "'") {
+                $suffix = ',';
+            } elseif (substr($value, -1) == ',') {
+                // Should be single quoted, if ends with comma.
+            } else {
+                $prefix = "'";
+                $suffix = "',";
+            }
+
+            $output .= "\n\t\t" . str_pad("'$name'", 18) . '=> ' . $prefix . $value . $suffix . "\n\n";
+        }
+
+        $output .= $line;
+    }
+
+    /**
+     * Save the file.
+     */
+    if (!$in_array) {
+        file_put_contents($file, $output);
+    }
