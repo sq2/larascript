2d51db875a940b2e5b8203506c0f407f729b02d2
Matthew Hamel <matt@codepl.us>
2014-02-07 19:42:24 -0500

    Add Bash helper functions

---
+++ helpers/functions.sh
@@ -0,0 +1,12 @@
+add_service_provider () {
+    php "$SOURCE_PATH"/helpers/add_service_provider.php "$1" "$WORK_PATH/${2:-app/config/app.php}"
+}
+
+add_alias () {
+    php "$SOURCE_PATH"/helpers/add_alias.php "$1" "$2" "$WORK_PATH/${3:-app/config/app.php}"
+}
+
+# Usage: add_to_composer ".autoload.psr-0.Helpers.app/lib"
+add_to_composer () {
+    php "$SOURCE_PATH"/helpers/add_to_json.php "$1" "${2:-key}" "$WORK_PATH/${3:-composer.json}"
+}
